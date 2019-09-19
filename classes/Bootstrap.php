69fad2df0ee883c63e97a835b2bb9256bb56dad2
assyk <assendk@gmail.com>
2019-09-19 22:46:42 +0300

    base classes and controllers

---
+++ classes/Bootstrap.php
@@ -0,0 +1,46 @@
+<?php
+class Bootstrap{
+    private $controller;
+    private $action;
+    private $request;
+
+    public function __construct($request){
+        $this->request = $request;
+        if($this->request['controller'] == ""){
+            $this->controller = 'home';
+        } else {
+            $this->controller = $this->request['controller'];
+        }
+
+        if($this->request['action'] == ""){
+            $this->action = 'index';
+        } else {
+            $this->action = $this->request['action'];
+        }
+//        echo $this->controller;
+    }
+
+    public function createController(){
+        if(class_exists($this->controller)){
+            $parents = class_parents($this->controller);
+
+            if(in_array("Controller", $parents)){
+                if(method_exists($this->controller, $this->action)){
+
+                    return new $this->controller($this->action, $this->request);
+
+                } else {
+                    echo 'Method does not exist';
+                    return;
+                }
+            } else {
+                echo 'Base controller not found';
+                return;
+            }
+        } else {
+            echo 'Controller class does not exist';
+            return;
+        }
+
+    }
+}
---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+require('config.php');
+
+require('classes/Bootstrap.php');
+require ('classes/Controller.php');
+
+require ('controllers/home.php');
+require ('controllers/users.php');
+
+$bootstrap = new Bootstrap($_GET);
+$controller = $bootstrap->createController();
+
+if($controller) {
+    $controller->executeAction();
+}
